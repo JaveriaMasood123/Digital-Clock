77e0970b65dcdbb43d7977f83587e48175dcde5c
Javeria Masood <javeriamails@gmail.com>
2025-04-30 23:16:08 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+setInterval(()=>{
+document.getElementById("time").textContent=new Date().toLocaleTimeString();
+}, 1000);
+
+ 
+//explanation
+// setInterval(()=>{
+//     console.log("hello world");
+// },1000);     counter show with hello world.
+
+// let date=new Date();
+// console.log(date);  //show complete information about time.
+// console.log(date.toLocaleTimeString());  //only show 5:44:27 PM this.
+
+// const jiaa=document.getElementById("jia");
+// console.log(jiaa);  //show <p id="jia">jia rajput</p>
+// console.log(jiaa.textContent);  //show jia rajput
+
+
+
+
